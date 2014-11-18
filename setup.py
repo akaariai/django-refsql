5032248731a8c43008b97f2ee6e17be9f481c714
Anssi Kääriäinen <akaariai@gmail.com>
2014-11-18 15:53:55 +0200

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+# -*- encoding: utf-8 -*-
+import os
+from setuptools import setup
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+setup(
+    name="django-refsql",
+    version="0.1-dev",
+    description="Raw SQL with lookups for Django",
+    long_description=read('readme.rst'),
+    url='https://github.com/akaariai/django-refsql',
+    license='MIT',
+    author=u'Anssi Kääriäinen',
+    author_email='akaariai@gmail.com',
+    classifiers=[
+        'Development Status :: 2 - Pre-Alpha',
+        'Environment :: Console',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python',
+    ],
+    packages=['django_refsql']
+)
